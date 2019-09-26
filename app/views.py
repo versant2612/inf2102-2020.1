05005198bb8277370291a236a3b42b5388f3718f
Mariana Salgueiro <marianadsalgueiro@gmail.com>
2019-09-25 21:37:11 -0300

    novo requirements; conexao (superficial) com allegro

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 # views.py
+from franz.openrdf.sail.allegrographserver import AllegroGraphServer
+from franz.openrdf.repository.repository import Repository
 
 from flask import render_template
 
@@ -6,8 +8,13 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+	server = AllegroGraphServer(host="localhost", port=10035, user="nima", password="nima2019")
+	catalog = server.openCatalog()
+	print(catalog.getRepository("lattes", Repository.ACCESS).getConnection().isEmpty())
+	print(catalog.getRepository("lattes", Repository.ACCESS).getConnection().listIndices())
+
+	return render_template("index.html")
 
 @app.route('/about')
 def about():
-    return render_template("about.html")
+	return render_template("about.html")
