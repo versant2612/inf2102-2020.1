9aa4dfd3836beff90b442cde0eb173335cf523ac
Veronica dos Santo <versant.2612@gmail.com>
2020-02-13 16:05:32 -0300

    v2.1
    Ajuste de caminho nos arquivos

--- carga/xml2rdf_alunos.sh
+++ carga/xml2rdf_alunos.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd lattes-alunos-xml/
-
 echo "Iniciando o script:"
+cd lattes-alunos-xml/
 
 echo "Convertendo os xmls em rdfs..."
 for f in *.xml
@@ -10,7 +9,10 @@ do
     newFile=${f/${f: -4}/} 
 
     # need to install libxml2, libxslt
-    xsltproc --stringparam ID $newFile lattes_alunos.xsl $f > $newFile".rdf"
+    xsltproc --stringparam ID $newFile ../lattes_alunos.xsl $f > $newFile".rdf"
 done
+
+echo "Movendo rdfs para diretorio separado ..."
+mv *.rdf ../lattes-alunos-rdf/
 
 
--- carga/xml2rdf_professores.sh
+++ carga/xml2rdf_professores.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd lattes-professores-xml/
-
 echo "Iniciando o script:"
+cd lattes-professores-xml/
 
 echo "Convertendo os xmls em rdfs..."
 for f in *.xml
@@ -10,7 +9,11 @@ do
     newFile=${f/${f: -4}/} 
 
     # need to install libxml2, libxslt
-    xsltproc --stringparam ID $newFile lattes_professores.xsl $f > $newFile".rdf"
+    xsltproc --stringparam ID $newFile ../lattes_professores.xsl $f > $newFile".rdf"
 done
+
+echo "Movendo rdfs para diretorio separado ..."
+find . -name "*.rdf" -type f | head -546 | while read arq; do mv "$arq" ../lattes-professores-rdf/; done;
+mv *.rdf ../lattes-professores2-rdf/
 
 
